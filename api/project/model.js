ed892daaa7c14180924e89d1f118dc489421d045
james starling <e.s.gbeast@gmail.com>
2023-03-15 13:41:20 -0400

    changes

--- api/project/model.js
+++ api/project/model.js
@@ -1,14 +1,42 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig')
 
-const db = require('../../data/dbConfig.js')
-
-function getProjects() {
-    return db('projects');
+async function get() {
+    const rows = await db('projects');
+    const result = rows.map(row => {
+        return {
+            ...row,
+            project_completed: row.project_completed ? true : false
+        }
+    })
+    return (result)
 }
 
-async function insert(project) {
-    const [project_id] = await db('projects').insert(project);
-    return db('projects').where({ project_id }).first();
+async function create(project) {
+    const [newID] = await db('projects').insert(project)
+    const newPost = await db('projects').where('project_id', newID)
+    const result = newPost.map(row => {
+        return {
+            ...row,
+            project_completed: row.project_completed ? true : false
+        }
+    })
+    return result[0]
 }
 
-module.exports = { getProjects, insert };
+async function getById(id) {
+    try {
+        const row = await db('projects')
+            .where('project_id', id)
+        return (row)
+    } catch (error) {
+        return
+    }
+
+}
+
+module.exports = {
+    get,
+    create,
+    getById
+}
--- api/server.js
+++ api/server.js
@@ -1,24 +1,25 @@
 // build your server here and require it from index.js
-const express = require('express');
-
-const projectsRouter = require('./project/router');
-const resourceRouter = require('./resource/router');
-const taskRouter = require('./task/router');
+const express = require('express')
+const resourcesRouter = require('./resource/router')
+const projectsRouter = require('./project/router')
+const tasksRouter = require('./task/router')
 
 const server = express();
 
 server.use(express.json());
-server.use('/api/projects', projectsRouter);
-server.use('/api/resources', resourceRouter);
-server.use('/api/tasks', taskRouter);
+server.use('/api/resources', resourcesRouter)
+server.use('/api/projects', projectsRouter)
+server.use('/api/tasks', tasksRouter)
 
-server.use('*', (req, res) => {
-    res.json({ api: 'ready' })
+server.get('/', (req,res) => {
+    res.status(200).json({ message: 'connected to server!'})
 })
 
+// eslint-disable-next-line no-unused-vars
 server.use((err, req, res, next) => {
-    res.status(err.status || 500).json({ message: err.message })
-    next()
-  })
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
 
-module.exports = server;
+module.exports = server
